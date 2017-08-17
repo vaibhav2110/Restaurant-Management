2d14047425fc3f3bb96b08ffc06ed807a9a7e221
Vaibhav Prakash <vaibhpraky@gmail.com>
2017-08-17 23:35:04 +0530

    grid and cards

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,14 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-
-    dishes: Dish[] = [
+const DISHES: Dish[] =  [
                          {
                            name:'Uthappizza',
                            image: '/assets/images/uthappizza.png',
@@ -39,6 +32,17 @@ export class MenuComponent implements OnInit {
                            price:'2.99',
                            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
                         ];
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+    dishes: Dish[] = DISHES;
+    selectedDish: Dish = DISHES[0];
 
     constructor() { }
 
