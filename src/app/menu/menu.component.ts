12eab18528f757e7e82e103bd44d419639612cbf
Vaibhav Prakash <vaibhpraky@gmail.com>
2017-09-29 17:31:52 +0530

    services

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,38 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
-
-const DISHES: Dish[] =  [
-                         {
-                           name:'Uthappizza',
-                           image: '/assets/images/uthappizza.png',
-                           category: 'mains',
-                           label:'Hot',
-                           price:'4.99',
-                           description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-                        {
-                           name:'Zucchipakoda',
-                           image: '/assets/images/zucchipakoda.png',
-                           category: 'appetizer',
-                           label:'',
-                           price:'1.99',
-                           description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-                        {
-                           name:'Vadonut',
-                           image: '/assets/images/vadonut.png',
-                           category: 'appetizer',
-                           label:'New',
-                           price:'1.99',
-                           description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-                        {
-                           name:'ElaiCheese Cake',
-                           image: '/assets/images/elaicheesecake.png',
-                           category: 'dessert',
-                           label:'',
-                           price:'2.99',
-                           description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-                        ];
-
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -41,12 +10,17 @@ const DISHES: Dish[] =  [
 })
 export class MenuComponent implements OnInit {
 
-    dishes: Dish[] = DISHES;
-    selectedDish: Dish = DISHES[0];
+    dishes: Dish[];
+    selectedDish:Dish;
 
-    constructor() { }
+    constructor(private dishService: DishService) { }
 
     ngOnInit() {
+      this.dishes = this.dishService.getDishes();
+    }
+
+    onSelect(dish:Dish){
+      this.selectedDish = dish;
     }
 
 }
